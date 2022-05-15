44e48929bbf8a82dd37fb84b066d660bfeae8e71
quytr <quy.truong310@gmail.com>
2022-05-15 08:57:02 -0400

    clone print 1 -50

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        for (int i = 1; i < 101; i++) {
+        for (int i = 1; i < 51; i++) {
             System.out.println(i);
         }
     }
