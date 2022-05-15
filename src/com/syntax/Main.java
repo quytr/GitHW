c161435ce483ff4a2d15a3ec3abbd4365e633c12
quytr <quy.truong310@gmail.com>
2022-05-15 08:52:14 -0400

    initial commit

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,12 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        for (int i = 1; i < 101; i++) {
+            System.out.println(i);
+        }
+    }
+}
